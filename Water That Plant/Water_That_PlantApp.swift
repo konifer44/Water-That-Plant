b8ba051e4aae11a70bd451393ff0cebcca8c4dac
Jan Konieczny <konifer44@gmail.com>
2023-06-03 10:32:22 +0200

    Initial Commit

---
+++ Water That Plant/Water_That_PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_That_PlantApp.swift
+//  Water That Plant
+//
+//  Created by Jan Konieczny on 03.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Water_That_PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
