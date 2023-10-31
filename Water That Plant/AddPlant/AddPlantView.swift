3c1a185961df205905e4da455c3ecec99c40cb02
Jan Konieczny <jan.konieczny@primesoft.pl>
2023-10-31 08:06:36 +0100

    Zmiany

--- Water That Plant/AddPlant/AddPlantView.swift
+++ Water That Plant/AddPlant/AddPlantView.swift
@@ -20,115 +20,79 @@ struct AddPlantView: View {
     }
     
     var body: some View {
-        
-        GeometryReader { geometry in
-            VStack(spacing: 0) {
-                ZStack(alignment: .bottom) {
-                    Image(uiImage: viewModel.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: geometry.size.width, height: 300)
-                        .clipped()
-                    
-                    Rectangle()
-                        .fill(Color(uiColor: .systemGray6))
-                        .mask(gradient)
-                    
-                    HStack(alignment: .bottom) {
-                        VStack(alignment: .leading) {
-                            Text(viewModel.name)
-                                .font(.title)
-                                .bold()
-                                .foregroundColor(.oliveGreen)
-                            Text(viewModel.selectedRoom.rawValue)
-                                .font(.headline)
-                                .bold()
-                                .foregroundColor(Color(uiColor: .systemGray2))
-                        }
-                        .padding()
-                        Spacer()
-                    }
-                }
-                .frame(height: 300)
-                
-                List {
-                    Section("Name"){
-                        TextField(viewModel.name.isEmpty ? "Enter name" : viewModel.name, text: $viewModel.name)
-                            .modifier(TextFieldClearButton(text: $viewModel.name))
-                            .foregroundColor(.gray)
-                            .padding(.bottom, 0.5)
-                    }
-                    Section("plant requirements"){
-                        Picker("Humidity", selection: $viewModel.recommendedHumidity){
-                            ForEach(RecommendedHumidityType.allCases, id: \.self){
-                                Text("\($0.range.lowerBound)-\($0.range.upperBound)%")
-                            }
-                        }
-                        
-                        Picker("Lighting", selection: $viewModel.recommendedLighting){
-                            ForEach(RecommendedLightingType.allCases, id: \.self){
-                                Text("\($0.range.lowerBound)-\($0.range.upperBound)%")
-                            }
-                        }
-                        Picker("Fertilization", selection: $viewModel.recommendedFertilization){
-                            ForEach(RecommendedFertilizationType.allCases, id: \.self){
-                                Text($0.rawValue)
-                            }
-                        }
-                    }
-                    Section("Details"){
-                        Picker("Room", selection: $viewModel.selectedRoom) {
-                            ForEach(RoomsType.allCases, id: \.self) {
-                                Text($0.rawValue).tag($0)
-                            }
-                        }
-                        
-                        DatePicker("Date of buy", selection: $viewModel.dateOfBuy, in: ...Date(), displayedComponents: .date)
-                        
-                        Toggle("Is favourite", isOn: $viewModel.isFavourite)
-                    }
-                    
-                    
-                    Section("Sensor"){
-                        NavigationLink("Select sensor"){
-                            SensorsListView(bindedPeripheralIdentifier: $viewModel.peripheralUUID, alertItemTitle: viewModel.name)
-                        }
-                    }
-                    
-                    Section{
-                        PhotosPicker(
-                            selection: $viewModel.selectedPhotoItem,
-                            matching: .images,
-                            photoLibrary: .shared()) {
-                                Text(viewModel.selectedPhoto == nil ? "Select photo" : "Change photo")
-                            }
-                    }
-                }
-                
-                .toolbarBackground(.teal)
-                .navigationTitle(viewModel.title)
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button("Cancel", role: .destructive) {
-                            // viewModel.clearView()
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Save"){
-                            viewModel.savePlant { success, error in
-                                viewModel.objectWillChange.send()
-                                dismiss()
-                            }
-                        }
-                    }
-                }
-                
+        List {
+            Section("Name"){
+                TextField(viewModel.name.isEmpty ? "Enter name" : viewModel.name, text: $viewModel.name)
+                    .modifier(TextFieldClearButton(text: $viewModel.name))
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 0.5)
             }
-            .edgesIgnoringSafeArea(.top)
+            Section("plant requirements"){
+                Picker("Humidity", selection: $viewModel.recommendedHumidity){
+                    ForEach(RecommendedHumidityType.allCases, id: \.self){
+                        Text("\($0.range.lowerBound)-\($0.range.upperBound)%")
+                    }
+                }
+                
+                Picker("Lighting", selection: $viewModel.recommendedLighting){
+                    ForEach(RecommendedLightingType.allCases, id: \.self){
+                        Text("\($0.range.lowerBound)-\($0.range.upperBound)%")
+                    }
+                }
+                Picker("Fertilization", selection: $viewModel.recommendedFertilization){
+                    ForEach(RecommendedFertilizationType.allCases, id: \.self){
+                        Text($0.rawValue)
+                    }
+                }
+            }
+            Section("Details"){
+                Picker("Room", selection: $viewModel.selectedRoom) {
+                    ForEach(RoomsType.allCases, id: \.self) {
+                        Text($0.rawValue).tag($0)
+                    }
+                }
+                
+                DatePicker("Date of buy", selection: $viewModel.dateOfBuy, in: ...Date(), displayedComponents: .date)
+                
+                Toggle("Is favourite", isOn: $viewModel.isFavourite)
+            }
+            
+            
+            Section("Sensor"){
+                NavigationLink("Select sensor"){
+                    SensorsListView(bindedPeripheralIdentifier: $viewModel.peripheralUUID, alertItemTitle: viewModel.name)
+                }
+            }
+            
+            Section{
+                PhotosPicker(
+                    selection: $viewModel.selectedPhotoItem,
+                    matching: .images,
+                    photoLibrary: .shared()) {
+                        Text(viewModel.selectedPhoto == nil ? "Select photo" : "Change photo")
+                    }
+            }
         }
         
+        .toolbarBackground(.teal)
+        .navigationTitle(viewModel.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("Cancel", role: .destructive) {
+                    // viewModel.clearView()
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Save"){
+                    viewModel.savePlant { success, error in
+                        viewModel.objectWillChange.send()
+                        dismiss()
+                    }
+                }
+            }
+        }    
     }
     
     let gradient = LinearGradient(
@@ -142,9 +106,17 @@ struct AddPlantView: View {
 }
 
 struct AddPlantView_Previews: PreviewProvider {
+    @Binding var isPresented = true
     static var previews: some View {
+      
         NavigationStack{
-            AddPlantView(viewModel: AddPlantViewModel())
+            
+         
+        }.sheet(isPresented: $isPresented){
+            NavigationStack{
+                AddPlantView(viewModel: AddPlantViewModel())
+                    .navigationTitle("Add plant")
+            }
         }
         
     }
--- Water That Plant/PlantList/PlantsList.swift
+++ Water That Plant/PlantList/PlantsList.swift
@@ -23,93 +23,113 @@ struct PlantList: View {
         self.viewModel = viewModel
     }
     
+    @State private var showRecentAlertsSection: Bool = true
     var body: some View {
-        ScrollView {
-            Divider()
-            HStack {
-                Text("Most recent alerts")
-                    .font(.headline)
-                Spacer()
-            }
-            .padding(.leading, 17)
-            
-            ScrollView(.horizontal){
-                HStack{
-                    ForEach(plants) { plant in
-                        NavigationLink {
-                            PlantDetailView(plant: plant)
-                        } label: {
-                            PlantListTileView(plant: plant)
-                            
+            ScrollView {
+                Divider()
+                HStack {
+                    Text("Most recent alerts")
+                        .font(.headline)
+                    Spacer()
+                    Button{
+                        withAnimation {
+                            showRecentAlertsSection.toggle()
                         }
-                        
-                        .frame(width: 350, height: 150)
-                        .padding(EdgeInsets(top: 7, leading: 7, bottom: 7, trailing: 7))
-                        .buttonStyle(.plain)
-                        .swipeActions {
-                            editAction
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundStyle(.white, Color.oliveGreen)
+                    }
+                   
+                }
+                .padding(EdgeInsets(top: 7, leading: 17, bottom: 7, trailing: 20))
+                if showRecentAlertsSection {
+                    ScrollView(.horizontal){
+                        HStack{
+                            ForEach(plants) { plant in
+                                NavigationLink {
+                                    PlantDetailView(plant: plant)
+                                } label: {
+                                    PlantListTileView(plant: plant)
+                                    
+                                }
+                                
+                                .frame(width: 350, height: 150)
+                                .padding(EdgeInsets(top: 7, leading: 7, bottom: 7, trailing: 7))
+                                .buttonStyle(.plain)
+                                .swipeActions {
+                                    editAction
+                                }
+                            }
                         }
                     }
+                    //.contentMargins(.bottom, 7, for: .scrollContent)
+                    .contentMargins([.leading, .bottom], 7, for: .scrollContent)
                 }
-            }
-            //.contentMargins(.bottom, 7, for: .scrollContent)
-            .contentMargins([.leading, .bottom], 7, for: .scrollContent)
-            
-            Divider()
-            HStack {
-                Text("All plants")
-                    .font(.headline)
-                Spacer()
-                Menu {
-                    Text("Sort by:")
-                    Picker("Sort", selection: $viewModel.sortDescriptor, content: {
-                        
-                        ForEach(SortDescriptors.allCases, id: \.self){
-                            Text($0.rawValue).tag($0)
-                        }
-                    })
-                } label: {
-                    Image(systemName: "line.3.horizontal.decrease.circle")
-                        .foregroundStyle(Color.oliveGreen)
+                Divider()
+                HStack {
+                    Text("All plants")
+                        .font(.headline)
+                    Spacer()
+                    Menu {
+                        Text("Sort by:")
+                        Picker("Sort", selection: $viewModel.sortDescriptor, content: {
+                            
+                            ForEach(SortDescriptors.allCases, id: \.self){
+                                Text($0.rawValue).tag($0)
+                            }
+                        })
+                    } label: {
+                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                            .foregroundStyle(.white, Color.oliveGreen)
+                    }
                 }
-            }
-            .padding(EdgeInsets(top: 7, leading: 17, bottom: 7, trailing: 20))
-                
-            ForEach(plants) { plant in
-                NavigationLink {
-                    PlantDetailView(plant: plant)
-                } label: {
-                    PlantListTileView(plant: plant)
+                .padding(EdgeInsets(top: 7, leading: 17, bottom: 7, trailing: 20))
                     
+                ForEach(plants) { plant in
+                    NavigationLink {
+                        PlantDetailView(plant: plant)
+                    } label: {
+                        PlantListTileView(plant: plant)
+                        
+                    }
+                    
+                    .frame(height: 110)
+                    .padding(EdgeInsets(top: 7, leading: 17, bottom: 7, trailing: 17))
+                    .buttonStyle(.plain)
+                    .swipeActions {
+                        editAction
+                    }
                 }
                 
-                .frame(height: 110)
-                .padding(EdgeInsets(top: 7, leading: 17, bottom: 7, trailing: 17))
-                .buttonStyle(.plain)
-                .swipeActions {
-                    editAction
+                .listStyle(.plain)
+                .onChange(of: viewModel.sortDescriptor) { oldSortDescriptor, newSortDescriptor in
+                    setSortDescriptor(to: newSortDescriptor)
+                }
+                
+                .sheet(item: $viewModel.addPlantViewModel) { viewModel in
+                    AddPlantView(viewModel: viewModel)
                 }
             }
             
-            .listStyle(.plain)
-            .onChange(of: viewModel.sortDescriptor) { oldSortDescriptor, newSortDescriptor in
-                setSortDescriptor(to: newSortDescriptor)
+            .navigationTitle("Your plants")
+            .edgesIgnoringSafeArea(.bottom)
+            .gradientBackground()
+            .toolbarBackground(.ultraThinMaterial, for: .navigationBar, .tabBar)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image("fikus")
+                            .resizable()
+                            .frame(width: 40, height: 40, alignment: .center)
+                            .clipShape(Circle())
+                            .shadow(color: .black.opacity(0.3), radius: 5)
+                    }
+
+                   
+                }
             }
-            
-            .sheet(item: $viewModel.addPlantViewModel) { viewModel in
-                AddPlantView(viewModel: viewModel)
-            }
-        }
-        
-        .navigationTitle("Your plants")
-        .edgesIgnoringSafeArea(.bottom)
-        .gradientBackground()
-        .toolbarBackground(.ultraThinMaterial, for: .navigationBar, .tabBar)
-        .toolbar{
-            ToolbarItem(placement: .topBarTrailing) {
-         
-            }
-        }
     }
     
    
